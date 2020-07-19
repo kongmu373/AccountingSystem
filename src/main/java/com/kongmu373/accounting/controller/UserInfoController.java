3453f4ef71097750ddedb6eef71585a8c32f683e
kongmu373 <kongmu1235@gmail.com>
2020-07-19 09:40:31 +0800

    chore(swagger2): add swagger2 to project.

--- src/main/java/com/kongmu373/accounting/controller/UserInfoController.java
+++ src/main/java/com/kongmu373/accounting/controller/UserInfoController.java
@@ -1,11 +1,16 @@
 package com.kongmu373.accounting.controller;
 
 import com.kongmu373.accounting.converter.c2s.UserInfoC2SConverter;
+import com.kongmu373.accounting.exception.ErrorResponse;
 import com.kongmu373.accounting.exception.InvalidParameterException;
 import com.kongmu373.accounting.model.service.UserInfoVo;
 import com.kongmu373.accounting.service.UserInfoService;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +24,13 @@ import java.util.Objects;
 @RestController
 @RequestMapping("v1.0/users")
 @Slf4j
+@Api(value = "UserInfo API")
 public class UserInfoController {
     private final UserInfoC2SConverter userInfoC2SConverter;
     private final UserInfoService userInfoService;
+    private static final int OK = 200;
+    private static final int BAD_REQUEST = 400;
+    private static final int NOT_FOUND = 404;
 
     @Autowired
     public UserInfoController(UserInfoC2SConverter userInfoC2SConverter, UserInfoService userInfoService) {
@@ -35,6 +44,11 @@ public class UserInfoController {
      * @param id the user id
      * @return userInfo response entity
      */
+    @ApiOperation(value = "get UserInfo by pass UserId", response = UserInfoVo.class)
+    @ApiResponses(value = {
+        @ApiResponse(code = OK, message = "获取用户信息成功", response = UserInfoVo.class),
+        @ApiResponse(code = BAD_REQUEST, message = "输入参数有误", response = ErrorResponse.class),
+        @ApiResponse(code = NOT_FOUND, message = "用户信息没有找到", response = ErrorResponse.class)})
     @GetMapping("/{id}")
     @SuppressFBWarnings("NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE")
     public ResponseEntity<UserInfoVo> getUserInfoByUserId(@PathVariable("id") long id) {
